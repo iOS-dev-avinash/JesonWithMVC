1887142c6b6b176694acd54b96ae19d9a67c1968
Admin <admin@Mac-Admin.local>
2017-10-27 20:50:28 +0300

    Initial Commit

---
+++ JsonExampleWithMVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsonExampleWithMVC
+//
+//  Created by Admin on 05/08/1939 Saka.
+//  Copyright © 1939 Saka Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
